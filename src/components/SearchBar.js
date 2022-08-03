c95b9d2073fbd6d80311cd9976a110df23fff8c1
flash-of-genius <amhdaar@gmail.com>
2022-08-03 15:36:44 +0100

    ef7f0c4: 14 - Solving Context Issues : second solution

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,18 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
-  onFormSubmit = (event) => {
+  onFormSubmit(event) {
     event.preventDefault();
     console.log(this.state.term);
-  };
+  }
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} action="" className="ui form">
+        <form
+          onSubmit={(e) => this.onFormSubmit(e)}
+          action=""
+          className="ui form"
+        >
           <div className="field">
             <label htmlFor="">Image Search</label>
 
