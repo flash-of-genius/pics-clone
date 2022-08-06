1fa435d28319a54194bdfec9497aac407e63ccc5
flash-of-genius <amhdaar@gmail.com>
2022-08-06 08:13:07 +0100

    5 - Setting State After Async Requests and 6 - Binding Callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,21 @@ import SearchBar from "./SearchBar";
 import axios from "axios";
 
 class App extends React.Component {
-  async onSeachSubmit(term) {
+  state = { images: [] };
+  onSeachSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID X-O-k78uah8ifc4EGVo-LV0f5zHoZdWVVHhToxNrQt8",
       },
     });
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSeachSubmit} />
+        Found: {this.state.images.length} images!
       </div>
     );
   }
