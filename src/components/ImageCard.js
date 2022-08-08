7f82c4fb501eda5f3a00f9edfb3c58f5f90f93a2
flash-of-genius <amhdaar@gmail.com>
2022-08-08 07:43:41 +0100

    5 - Accessing Image Height

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+  componentDidMount() {
+    console.log(this.imageRef.current.clientHeight);
+  }
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img src={urls.regular} alt={description} />
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
