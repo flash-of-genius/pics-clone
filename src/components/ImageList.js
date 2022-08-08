a29270645ad907d877c839ed6691a928a2d5a7ab
flash-of-genius <amhdaar@gmail.com>
2022-08-08 06:30:23 +0100

    1 - Grid CSS

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,7 +4,7 @@ const ImageList = (props) => {
   const images = props.images.map(({ description, id, urls }) => {
     return <img key={id} src={urls.regular} alt={description} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
